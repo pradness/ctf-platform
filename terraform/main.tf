9231c69de11d2ac2643922bca48dfbc81a01cfa2
pradness <pradnyesh.keluskar23@spit.ac.in>
2026-04-30 21:11:42 +0530

    jenkinsss

--- terraform/main.tf
+++ terraform/main.tf
@@ -96,6 +96,12 @@ resource "aws_security_group" "ec2_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
