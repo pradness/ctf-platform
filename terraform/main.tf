d3d4beba2eb17321eefbe023d0ca71f1d9aa85dc
pradness <pradnyesh.keluskar23@spit.ac.in>
2026-05-03 11:31:11 +0530

    sqli container fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -102,6 +102,12 @@ resource "aws_security_group" "ec2_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 4000
+    to_port     = 4000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
